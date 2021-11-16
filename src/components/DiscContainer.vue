75637eaa7f38933dbd71fc8b532fe20051198787
yanitejeda <tejedasp@gmail.com>
2021-11-16 22:42:27 +0100

    creato filtro manca visualizzare geere

--- src/components/DiscContainer.vue
+++ src/components/DiscContainer.vue
@@ -2,8 +2,17 @@
     <div>
         <div class="topBg py-2"> <img class="mw3" :src="require('@/assets/spotify-log.png')" alt=""></div>
         <div class="mainbg py-2" > 
+
+                <div class="py-5">
+                    <select class="form-select with20 mx-auto" aria-label="Default select example ">
+                        <option selected>scegli genere musicale</option>
+                        <option v-for="(value, key) in getGenreList " :key="key">{{key}} n. {{value}}</option>
+                    </select>
+                </div>
+           
+            
             <SelecGenre>
-                
+
             </SelecGenre>
             <div class="row row-cols-1 row-cols-md-5 g-4 container mx-auto ">
                     <div class="col d-flex text-center" v-for="(disco, i) in listalbum" :key="i">
@@ -37,9 +46,58 @@ export default {
            listalbum:[]
        }
 
-   },
+    },
+    computed:{
+         getGenreList (){
+
+            const genreList = {};
+
+
+            this.listalbum.forEach((disco)=>{
+                const {genre} = disco;
+
+                if(!genreList[genre]) {
+
+                    genreList[genre] = 0;
+                }
+                    genreList[genre]++;
+            });
+
+        console.log( genreList);
+        return genreList
+
+        },
+
+    },
+
+    methods:{
+
+       /*  getGenreList (){
+
+            const genreList = {};
+
+
+            this.listalbum.forEach((disco)=>{
+                const {genre} = disco;
+
+                if(!genreList[genre]) {
+
+                    genreList[genre] = 0;
+                }
+                    genreList[genre]++;
+            });
+
+        console.log( genreList);
+        return genreList
+
+        }, */
+        
+    }, 
+    
+   
 
    mounted(){
+
    axios.get("https://flynn.boolean.careers/exercises/api/array/music")
    .then((resp) =>{
        this.listalbum = resp.data.response ;
--- src/components/SelecGenre.vue
+++ src/components/SelecGenre.vue
@@ -1,16 +1,24 @@
-<template>
+/* <template>
     <div class="py-5">
         <select class="form-select with20 mx-auto" aria-label="Default select example ">
             <option selected>scegli genere musucale</option>
-            <option value="1">One</option><!-- 
-            <option value="2">Two</option>
-            <option value="3">Three</option> -->
+            <option  value="1">{{genre}}</option>
         </select>
     </div>
 </template>
 <script>
 export default {
-    name: "SelecGenre"
-    
+    name: "SelecGenre",
+    props:["genre"]
+  /*   methods:{
+
+        getGenreList (){
+
+            const genreList = {},
+
+            this.
+        }
+    }
+     */
 }
-</script>
+</script> */
