79faeb3e6fcbdd885731dc3a3d10181bc7564ea3
yanitejeda <tejedasp@gmail.com>
2021-11-16 08:58:01 +0100

    modifiche

---
+++ src/components/DiscContainer.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div class="topBg py-2"> <img class="mw3" :src="require('@/assets/spotify-log.png')" alt=""></div>
+        <div class="mainbg py-5" > 
+            <div class="row row-cols-1 row-cols-md-5 g-4 container mx-auto ">
+                    <div class="col d-flex text-center" v-for="(disco, i) in listalbum" :key="i">
+                        <DiscoCard 
+                        :author="disco.author"
+                        :genre="disco.genre"
+                        :poster="disco.poster"
+                        :title="disco.title"
+                        :year="disco.year"
+                        >
+
+                        </DiscoCard>
+                   
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+<script>
+import axios from "axios";
+import DiscoCard from "./DiscoCard.vue"
+
+export default {
+   name:"DiscContainer",
+   components:{DiscoCard },
+
+   data(){
+       return{
+           listalbum:[]
+       }
+
+   },
+
+   mounted(){
+   axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+   .then((resp) =>{
+       this.listalbum = resp.data.response ;
+   })
+   },
+
+};
+</script>
---
+++ src/components/DiscoCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card bnone ">
+        <img :src="poster" class="card-img-top" alt="disco.title">
+        <div class="card-body cardBg">
+            <h3 class="card-title tittleColor">{{title}}</h3>
+
+            <p class="card-text textColor "><span>{{author}} <br> {{year}} </span></p>
+        </div>
+     </div>
+</template>
+
+<script>
+/* import DiscContainer from './DiscContainer.vue' */
+
+export default {
+    name:"DiscoCard",
+    props:[
+        'author','genre','poster','title','year'
+    ]
+}
+</script>
